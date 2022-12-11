e422d8f802ff6a3829ab7bfbe0811954d3f440e8
arielWhist <89840937+arielWhist@users.noreply.github.com>
2022-12-11 17:26:18 +0200

    Update app.groovy

--- app.groovy
+++ app.groovy
@@ -1,39 +1,22 @@
-agent {
-    kubernetes {
-        yaml '''
-apiVersion: v1
-kind: Pod
-metadata:
-spec:
-    containers:
-    - name: $label
-    image: busybox
-    command:
-    - cat
-    tty: true
-    imagePullPolicy: Always
-    volumeMounts:
-    - mountPath: /var/run/docker.sock
-        name: docker-sock
-    volumes:
-    - name: docker-sock
-    hostPath:
-        path: /var/run/docker.sock
-'''
+def choiceArray = []
+node {
+    checkout scm
+    def folders = sh(returnStdout: true, script: "ls $WORKSPACE")
+    
+    folders.split().each {
+        //condition to skip files if any
+        choiceArray << it
+    }
+}
+pipeline {
+    agent any;
+    parameters { choice(name: 'CHOICES', choices: choiceArray, description: 'Please Select One') }
+    stages {
+        stage('debug') {
+            steps {
+                echo "Selected choice is : ${params.CHOICES}"
+            }
         }
     }
- parameters {
-    string(name: "branch", defaultValue: 'main')
-    choice(name: "env", choices: ["test", "prod"], description: "Env for deployment")
-    booleanParam(name: "deploy_archive_scanner", value: false, description: "check the box if you would like to deploy archive scanner lambda")
-    booleanParam(name: "deploy_archive_file_extractor", value: false, description: "check the box if you would like to deploy archive file extractor lambda")
 }
-pipeline{
-    agent any;
- stages {
-    node("test") {
-            
-            echo "test"
-    }
- }
 }
