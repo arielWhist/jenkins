2fe3f20913febe0291b73efd70fd074b705a8e5a
arielWhist <89840937+arielWhist@users.noreply.github.com>
2022-12-12 10:46:02 +0200

    Update app.groovy

--- app.groovy
+++ app.groovy
@@ -1,25 +1,35 @@
-pipeline {
-  stages {
-  agent {
-    kubernetes {
-      yaml """
-kind: Pod
-spec:
-  containers:
-  - name: example-image
-    image: nginx:${params.AGENT_POD_SPEC}
-    imagePullPolicy: Always
-    command:
-    - ls
-"""
+parameters {
+    stringParam(name: "branch", defaultValue: 'main')
+    choiceParam(name: "env", choices: ["test", "prod"], description: "Env for deployment")
+    booleanParam(name: "deploy_archive_scanner", value: false, description: "check the box if you would like to deploy archive scanner lambda")
+    booleanParam(name: "deploy_archive_file_extractor", value: false, description: "check the box if you would like to deploy archive file extractor lambda")
+}
+
+podTemplate(
+        label: label,
+        cloud: "kubernetes",
+        yaml: """\
+    apiVersion: v1
+    kind: Pod
+    metadata:
+    spec:
+      containers:
+      - name: test
+        image: busybox
+        command:
+        - cat
+        tty: true
+        imagePullPolicy: Always
+        volumeMounts:
+        - mountPath: /var/run/docker.sock
+          name: docker-sock
+      volumes:
+      - name: docker-sock
+        hostPath:
+          path: /var/run/docker.sock
+    """.stripIndent()
+) {
+    node(label) {
+        print ${branch}
     }
-  }
-  parameters {
-    choice(
-      name: 'AGENT_POD_SPEC',
-      choices: ['latest','1.3.0','1.2.0','1.4.0'],
-      description: 'Agent pod configuration'
-    )
-  }
-  }
 }
