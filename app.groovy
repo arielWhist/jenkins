2c4f409654ae5d38c75cb4a71652e5e221d80700
arielWhist <89840937+arielWhist@users.noreply.github.com>
2022-12-12 10:51:57 +0200

    Update app.groovy

--- app.groovy
+++ app.groovy
@@ -5,30 +5,9 @@ parameters {
     booleanParam(name: "deploy_archive_file_extractor", value: false, description: "check the box if you would like to deploy archive file extractor lambda")
 }
 
-podTemplate(
-        cloud: "kubernetes",
-        yaml: """\
-    apiVersion: v1
-    kind: Pod
-    metadata:
-    spec:
-      containers:
-      - name: test
-        image: busybox
-        command:
-        - cat
-        tty: true
-        imagePullPolicy: Always
-        volumeMounts:
-        - mountPath: /var/run/docker.sock
-          name: docker-sock
-      volumes:
-      - name: docker-sock
-        hostPath:
-          path: /var/run/docker.sock
-    """.stripIndent()
-) {
-    node(label) {
-        print ${branch}
+podTemplate {
+    node(POD_LABEL) {
+        // pipeline steps...
+        print "echo"
     }
 }
